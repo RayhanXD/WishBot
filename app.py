d16e9f8b183e0f4779bc492cb15b406b52b48605
RayhanM <95842076+RayhanXD@users.noreply.github.com>
2024-06-02 11:46:28 -0500

    Nginx added

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ api_key_open = os.getenv('OPENAI_API_KEY')
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = '/Users/rayhanmohammad/cursor-ai-project/UPLOAD_FOLDER'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*", logger=True, engineio_logger=True)
 
 @app.route('/')
 def home():
